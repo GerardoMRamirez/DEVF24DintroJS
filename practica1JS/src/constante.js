16ef33fc7c59e4e65919cf039d8778013dee7848
GerardoMRamirez <treck8ramirez@gmail.com>
2023-04-06 13:36:21 -0600

    Variables, funciones y constantes

---
+++ practica1JS/src/constante.js
@@ -0,0 +1,5 @@
+const PI = 3.141592;
+
+PI = 3;
+
+console.log(PI);
---
+++ practica1JS/src/index.js
@@ -0,0 +1,17 @@
+//alert("Hola soy un comentsrio de una sola linea");
+
+/*
+console.log("Hola)
+console.log("soy")
+console.log("un comentario multilinea")
+*/
+
+// Un solo simbolo, es un caracter "char"
+//Los textos en programacion se conocen como "strings" o cadena de caracteres
+
+//cuando unimos varios strings con el simbolo de + se dice que estamos concatenando strings
+document.write("Titulo del documento" + "<br/>"); 
+document.write("Texto del documento");
+
+//prompt("Dime tu nombre");
+console.log ("Hola",prompt("Dime tu nombre"));
